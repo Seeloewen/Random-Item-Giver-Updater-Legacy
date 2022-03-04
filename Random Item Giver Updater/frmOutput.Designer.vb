9a53bf6f26235f4d35b421005e4d6530d8f1010d
Seeloewen <Louis.Herbert2006@gmail.com>
2022-03-05 00:42:13 +0100

    Add Output

--- Random Item Giver Updater/frmOutput.Designer.vb
+++ Random Item Giver Updater/frmOutput.Designer.vb
@@ -22,9 +22,28 @@ Partial Class frmOutput
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.rtbLog = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'rtbLog
+        '
+        Me.rtbLog.Location = New System.Drawing.Point(13, 13)
+        Me.rtbLog.Name = "rtbLog"
+        Me.rtbLog.Size = New System.Drawing.Size(775, 425)
+        Me.rtbLog.TabIndex = 0
+        Me.rtbLog.Text = ""
+        '
+        'frmOutput
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.rtbLog)
+        Me.Name = "frmOutput"
         Me.Text = "frmOutput"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents rtbLog As RichTextBox
 End Class
