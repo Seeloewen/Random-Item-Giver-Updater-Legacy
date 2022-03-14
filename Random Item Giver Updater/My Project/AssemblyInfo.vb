f584d6c14ddcb2c159798f13d0cf06d6160378ed
Seeloewen <Louis.Herbert2006@gmail.com>
2022-03-14 19:50:38 +0100

    Changed version number

--- Random Item Giver Updater/My Project/AssemblyInfo.vb
+++ Random Item Giver Updater/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' indem Sie "*" wie unten gezeigt eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.0.1.0")>
-<Assembly: AssemblyFileVersion("0.0.1.0")>
+<Assembly: AssemblyVersion("0.1.1.0")>
+<Assembly: AssemblyFileVersion("0.1.1.0")>
