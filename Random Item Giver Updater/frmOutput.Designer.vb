a284bd866012bcc7bb49bd1d6f849bf23356df57
Seeloewen <Louis.Herbert2006@gmail.com>
2022-05-02 18:17:09 +0200

    Add Output (prototype)

--- Random Item Giver Updater/frmOutput.Designer.vb
+++ Random Item Giver Updater/frmOutput.Designer.vb
@@ -22,28 +22,104 @@ Partial Class frmOutput
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmOutput))
         Me.rtbLog = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.gbLog = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.gbLog.SuspendLayout()
         Me.SuspendLayout()
         '
         'rtbLog
         '
-        Me.rtbLog.Location = New System.Drawing.Point(13, 13)
+        Me.rtbLog.BackColor = System.Drawing.Color.White
+        Me.rtbLog.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.rtbLog.Location = New System.Drawing.Point(6, 14)
         Me.rtbLog.Name = "rtbLog"
-        Me.rtbLog.Size = New System.Drawing.Size(775, 425)
+        Me.rtbLog.ReadOnly = True
+        Me.rtbLog.Size = New System.Drawing.Size(763, 405)
         Me.rtbLog.TabIndex = 0
         Me.rtbLog.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(551, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 31)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'gbLog
+        '
+        Me.gbLog.Controls.Add(Me.rtbLog)
+        Me.gbLog.Location = New System.Drawing.Point(12, 39)
+        Me.gbLog.Name = "gbLog"
+        Me.gbLog.Size = New System.Drawing.Size(775, 425)
+        Me.gbLog.TabIndex = 2
+        Me.gbLog.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(14, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 24)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Output Log"
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(632, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 31)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(713, 5)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 31)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Close"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'frmOutput
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.rtbLog)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 476)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.gbLog)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "frmOutput"
-        Me.Text = "frmOutput"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Output Log"
+        Me.gbLog.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents rtbLog As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents gbLog As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
