393faf559f02ce98641ffe5e24fbd16094474687
Seeloewen <Louis.Herbert2006@gmail.com>
2022-05-10 20:16:51 +0200

    Add logging

--- Random Item Giver Updater/frmItemImporter.vb
+++ Random Item Giver Updater/frmItemImporter.vb
@@ -9,7 +9,7 @@
         If My.Computer.FileSystem.FileExists(tbImportFromFile.Text) Then
             btnImport.Text = "Importing..."
             btnImport.Enabled = False
-
+            frmMain.WriteToLog("-- Importing item list --", "Info")
             rtbItems.Text = My.Computer.FileSystem.ReadAllText(tbImportFromFile.Text)
 
             'Remove text if file was generated using TellMe Mod
@@ -45,6 +45,7 @@
             btnImport.Text = "Import Item List"
             btnImport.Enabled = True
             MsgBox("Items were successfully imported!" + vbNewLine + "You can see them in the main window." + vbNewLine + vbNewLine + "Make sure to unselect 'Use the same prefix for all items'.", MsgBoxStyle.Information, "Import completed")
+            frmMain.WriteToLog("Successfully imported item list to main window.", "Info")
             Close()
         ElseIf String.IsNullOrEmpty(tbImportFromFile.Text) Then
             MsgBox("Please select a file in the format .txt or .csv!", MsgBoxStyle.Critical, "Error")
@@ -56,5 +57,6 @@
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles llbl.LinkClicked
         Clipboard.SetText("/tellme dump to-file csv items-registry-name-only")
         MsgBox("Copied the command to the clipboard!" + vbNewLine + "Paste it ingame with CTRL + V", MsgBoxStyle.Information, "Copied command")
+        frmMain.WriteToLog("Copied command for Item List Importer to clipboard.", "Info")
     End Sub
 End Class
